8810441f27f1b2ebe60e3aeaaffe318053e607a9
Aakash <jaswal.aakash.14ce1022@gmail.com>
2020-09-19 19:13:44 +0530

    helper libs

---
+++ helper.py
@@ -0,0 +1,13 @@
+# some lib which i think give an edge
+
+import timeit
+
+# let's check how much time it take to create a list 10000 time ?
+print(f'{timeit.timeit(stmt="[1,2,3,4,5]", number=10000)=} Sec')
+
+import sys
+
+lst = [1, 2, 3, 4, 5]
+print(f'{sys.getsizeof(lst)=} bytes')
+
+print(f'{id(lst)=}')
