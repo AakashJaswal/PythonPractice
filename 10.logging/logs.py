f86059a829288fd1bc914065602147cac5e00885
Aakash <jaswal.aakash.14ce1022@gmail.com>
2020-10-25 22:33:27 +0530

    removed todo

--- 10.logging/logs.py
+++ 10.logging/logs.py
@@ -2,7 +2,6 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S')
-import anotherone
 
 logging.debug('Debug Message')
 logging.info('Info Message')
@@ -10,4 +9,4 @@ logging.warning('Warning Message')
 logging.error('Error Message')
 logging.critical('Critical Message')
 
-# Todo: Look more into logger before 28th Sept
+import anotherone
