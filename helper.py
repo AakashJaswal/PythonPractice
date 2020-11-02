95051bff8e1687e2c157e8cae53ccf89639e72c7
Aakash <jaswal.aakash.14ce1022@gmail.com>
2020-11-03 03:46:50 +0530

    added decorator example

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 # some lib which i think give an edge
+import functools
 
 import timeit
 
@@ -14,3 +15,27 @@ print(f'{id(lst)=}')
 
 # Todo: Add about functional params
 # Todo: multithreading and multiprocessing
+
+
+def super_timer(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        print("Start Timing")
+        result = f(*args, **kwargs)
+        print(f"called function {f} with params {args}, it returned {result}")
+        print("End Timing")
+        return result
+    return wrapper
+
+@super_timer
+def add(a,b):
+    return a+b
+
+@super_timer
+def subtract(a,b):
+    return a-b
+
+print(add(2,3))
+print(add)
+print(subtract(2,3))
+print(subtract)
