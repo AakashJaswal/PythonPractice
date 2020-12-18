b41a8465fa2e24227caf1e62612e87bc4a08fea3
Aakash <jaswal.aakash.14ce1022@gmail.com>
2020-12-18 07:08:29 +0530

    enumerate in python

---
+++ 0.Book/Effective Python/Chapter1/item7.py
@@ -0,0 +1,8 @@
+# Using Enumerator
+sr = 'abcd'
+
+for i, char in enumerate(sr,start=1):
+    print(f"{i} {char}")
+
+# Enumarate wrape iterator with a lazy generator.
+# Return index and values/attribute/container
