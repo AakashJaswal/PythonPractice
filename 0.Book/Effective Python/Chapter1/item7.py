9c8d66240066ba511fc40f9d0ebd6e4261e868bf
Aakash <jaswal.aakash.14ce1022@gmail.com>
2020-12-18 07:10:56 +0530

    enumerate in python-desc

--- 0.Book/Effective Python/Chapter1/item7.py
+++ 0.Book/Effective Python/Chapter1/item7.py
@@ -5,4 +5,4 @@ for i, char in enumerate(sr,start=1):
     print(f"{i} {char}")
 
 # Enumarate wrape iterator with a lazy generator.
-# Return index and values/attribute/container
+# Return index and attribute in container
